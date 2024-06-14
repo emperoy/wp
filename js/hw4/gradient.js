24d8a0291d1b97d3280703e7bdd784fa4e15c61c
emperoy <81153704+emperoy@users.noreply.github.com>
2024-06-14 18:52:44 +0800

    Create gradient.js

---
+++ js/hw4/gradient.js
@@ -0,0 +1,28 @@
+const h = 0.01;
+
+// 我們想找函數 f 的最低點
+function f(p) {
+    const [x, y] = p;
+    return x * x + y * y;
+}
+
+
+function df(f, p, k) {
+    const p1 = p.slice(); 
+    p1[k] += h;
+    return (f(p1) - f(p)) / h;
+}
+
+function grad(f, p) {
+    const gp = p.slice(); 
+    for (let k = 0; k < p.length; k++) {
+        gp[k] = df(f, p, k);
+    }
+    return gp;
+}
+
+const [x, y] = [1, 3];
+console.log('x=', x, 'y=', y);
+console.log('df(f(x,y), 0) = ', df(f, [x, y], 0));
+console.log('df(f(x,y), 1) = ', df(f, [x, y], 1));
+console.log('grad(f)=', grad(f, [x, y]));
