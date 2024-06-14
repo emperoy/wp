28b68f9bf2418c98021fd16ce0388c01c85e005c
emperoy <81153704+emperoy@users.noreply.github.com>
2024-06-14 18:44:49 +0800

    Create 04.js

---
+++ js/hw3/04.js
@@ -0,0 +1,23 @@
+function isPrime(n){
+    if(n<=1){
+        return false;
+    }
+    for(var i=2;i<n;i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+function sumPrime(n){
+    var sum=0;
+    for(var j=1;j<=n;j++){
+        if(isPrime(j))
+            sum = sum + j;
+        
+    }
+    return sum;
+}
+var result = sumPrime(5);
+console.log("sum=",result);
